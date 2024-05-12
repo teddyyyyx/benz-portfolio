f23a132c77269e4d5cd167ae388801d0152dc57d
Benz Samson Tagle <benz@teddyyyyx.bbrouter>
2024-05-12 22:44:17 +0800

    Initial Commit. Added Navbar component

---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './intro.css';
+
+export const Intro = () => {
+  return (
+    <section id="intro">
+        <div className="introContent">
+            <span className="hello">Hello, </span>
+            <span className="introText">I'm <span className="introName">Benz </span><br /> Web Dev</span>
+            <p className="introPara">I am a </p>
+        </div>
+
+        <img src="" alt="" />
+    </section>
+)
+}
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './navbar.css';
+import logo from '../../assets/logo.png'
+import {Link} from 'react-scroll';
+import emailImg from '../../assets/email.png'
+
+export const Navbar = () => {
+  return (
+    <nav className="navbar">
+        <img src={logo} alt="logo" className='logo' />
+
+        <div className="desktopMenu">
+          <Link className="desktopMenuListItem">Home</Link>
+          <Link className="desktopMenuListItem">About</Link>
+          <Link className="desktopMenuListItem">Portfolio</Link>
+          <Link className="desktopMenuListItem">Clients</Link>
+        </div>
+        <button className="desktopMenuButton">
+            <img src={emailImg} alt="" className="desktopMenuImg" />
+            Contact Me
+        </button>
+    </nav>
+)
+}
+
+
