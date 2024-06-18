b1e08f207543d29ebaa96963753c1ac2ceaf8dcc
Benz Samson Tagle <benz@teddyyyyx.bbrouter>
2024-06-18 16:47:46 +0800

    Uploaded Hanzy Project. CSS adjusted

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import './projects.css'
-import testImg from '../../../src/assets/test-img.png'
+import testImg from '../../assets/test-img.png'
+import prj1 from '../../assets/project-imgs/hanzy-style_prj1.png'
+
 
 export const Projects = () => {
   return (
     <section id='projects'>
-      <h1 className=' title'>Recent Projects</h1>
+      <h1 className='title'>Recent Projects</h1>
 
 {/*  */}
 
       <div className='projects-container'>
-        <img src={testImg} alt="" className='project-img'/>
+        <a href="https://teddyyyyx.github.io/hanzy-stylez-react/" target='__blank'>
+          <img src={prj1} alt="" className='project-img'/>
+        </a>
 
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus odit quaerat saepe optio tempora architecto non cum reiciendis nam nostrum minima beatae cupiditate cumque, voluptatem vero doloribus! Optio, soluta est?
