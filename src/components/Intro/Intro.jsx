337e1036b9975290df9cc0ed3c81d1bca11b6e7b
Benz Samson Tagle <benz@teddyyyyx.bbrouter>
2024-06-19 18:07:06 +0800

    Minor UI margin revises

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -9,14 +9,9 @@ export const Intro = () => {
         <div className='intro-name-container'>
             <h1 className='intro-name animate-title'>Benz Samson Tagle</h1>
             <p className="intro-bio animate-title">Web Developer && Designer</p>
-            <p className='intro-extra animate-title'>(Google Search Expert <span> since birth</span>)</p>
             <p className='intro-description animate-title'>
                 Crafting ideas into reality, with a dash of code and a splash of imagination, aided with little bit of googling — one keystroke at a time
-
-                {/* I  am a <strong>Computer Science</strong> graduate specializing in 
-                <strong> Web Development</strong>, passionate about creating intuitive and 
-                responsive web applications that blend technical expertise with creative design.  */}
-            </p>
+                </p>
 
         </div>
 
