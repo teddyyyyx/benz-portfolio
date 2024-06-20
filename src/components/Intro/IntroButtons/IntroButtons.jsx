53c7bffb4a93ef1c3a643da1022a8feb51361dca
Benz Samson Tagle <benz@teddyyyyx.bbrouter>
2024-06-21 00:15:19 +0800

    Improved button ui

--- src/components/Intro/IntroButtons/IntroButtons.jsx
+++ src/components/Intro/IntroButtons/IntroButtons.jsx
@@ -7,14 +7,14 @@ export const IntroButtons = () => {
   return (
     <div className='buttons-container'> 
         <div className='cs-btn'>
-            <button className="contact-me-button" onClick={() => {
+            <button className="contact-me-button button" onClick={() => {
                 document.getElementById('contact').scrollIntoView({behavior: 'smooth'});
             }}>
                 <svg className="email-icon" xmlns="http://www.w3.org/2000/svg" height="34px" viewBox="0 -960 960 960" width="34px" fill="#111111"><path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280L160-640v400h640v-400L480-440Zm0-80 320-200H160l320 200ZM160-640v-80 480-400Z"/></svg>
                 Contact Me
             </button>
 
-            <button className='resume-button'> 
+            <button className='resume-button button'> 
                 <a href="/BenzTagle_resume.pdf" download>
                     <svg className='donwload-icon' xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#e8eaed"><path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"/></svg>
                     Resume
