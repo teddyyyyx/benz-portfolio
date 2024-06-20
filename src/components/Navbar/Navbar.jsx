f206434c33d2abc27fd8080815bd87c68fd89d31
Benz Samson Tagle <benz@teddyyyyx.bbrouter>
2024-06-21 03:44:17 +0800

    Fix spacing bugs

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,13 @@
 import React, { useState } from 'react'
 import './navbar.css';
-import logo from '../../assets/logo.png'
 import { Link } from 'react-scroll';
-import burgerImg from '../../assets/burgerImg.png'
 
 export const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
 
   return (
     <nav className="navbar">
-        <img src={logo} alt="logo" className='logo' />
+        <h1 className='logo-name'>benz.st</h1>
 
         <div className="desktopMenu">
           <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-350} duration={1000} className="desktopMenuListItem">Home</Link>
@@ -19,7 +17,15 @@ export const Navbar = () => {
           <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-50} duration={1000} className="desktopMenuListItem">Contact</Link>
         </div>
 
-        <img src={burgerImg} alt="Menu" className='burgerMenu' onClick={()=>setShowMenu(!showMenu)}/>
+        <svg className='burgerMenu' onClick={()=>setShowMenu(!showMenu)} width="70px" height="70px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#FFFFFF" transform="matrix(-1, 0, 0, 1, 0, 0)">
+            <g id="SVGRepo_bgCarrier" stroke-width="0"/>
+            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
+            <g id="SVGRepo_iconCarrier">
+            <path d="M5 12H18" stroke="#e7e3e3de" stroke-width="2" stroke-linecap="round"/>
+            <path d="M5 17H11" stroke="#e7e3e3de" stroke-width="2" stroke-linecap="round"/>
+            <path d="M5 7H15" stroke="#e7e3e3de" stroke-width="2" stroke-linecap="round"/>
+            </g>
+        </svg>
 
         <div className="navMenu" style={{display: showMenu? 'flex' : 'none'}}>
           <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-200} duration={500} className="listItem" onClick={() => setShowMenu(false)}>Home</Link>
