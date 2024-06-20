0143dba0a31b2f560d5fa81ff72f00bb0eafc48d
Benz Samson Tagle <benz@teddyyyyx.bbrouter>
2024-06-20 20:25:36 +0800

    Added HoneyFlix project

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './projects.css'
 import testImg from '../../assets/test-img.png'
 import prj1 from '../../assets/project-imgs/hanzy-style_prj1.png'
+import prj2 from '../../assets/project-imgs/HoneyFlix_prj2.png'
 
 
 export const Projects = () => {
@@ -16,23 +17,32 @@ export const Projects = () => {
         </a>
 
           <div>
+          <a href="https://teddyyyyx.github.io/hanzy-stylez-react/" target='__blank'>
             <h1>Hanzy Styles</h1>
+          </a>
             Developed the Hanzy Stylez e-commerce website with ReactJS, 
             crafting dynamic and responsive user interfaces for all modern devices. 
-            Applied my frontend expertise to ensure optimal performance and user experience."
+            Applied my frontend expertise to ensure optimal performance and user experience.
           </div>
       </div>
       <hr className='projects-hr'/>
 
-{/*  */}
+{/* */}
 
       <div className='projects-container col-reverse'>
         <div>
-          <h1>Project 2</h1>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus odit quaerat saepe optio tempora architecto non cum reiciendis nam nostrum minima beatae cupiditate cumque, voluptatem vero doloribus! Optio, soluta est?
+        <a href="https://teddyyyyx.github.io/movieflix-react/" target='__blank'> 
+          <h1>HoneyFlix</h1>
+        </a>
+          Crafted with love using ReactJS and fetched movie data from the Open Movie Database API. 
+          This website is designed for you to search your favorite movie and its information.
+          Ensuring compatibility and responsiveness that is truly
+          compatible for all modern viewports.          
         </div>
 
-        <img src={testImg} alt="" className='project-img1'/>
+        <a href="https://teddyyyyx.github.io/movieflix-react/" target='__blank'> 
+          <img src={prj2} alt="" className='project-img1'/>
+        </a>
       </div>
       <hr className='projects-hr'/>
 
