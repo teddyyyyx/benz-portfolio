bfb9837681c40e642b1cb424a930742d1fcabba2
teddyyyyx <benztagle123@Gmail.com>
2024-06-22 20:33:36 +0800

    Fixed burger nav space on right

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -24,7 +24,7 @@ export const Navbar = () => {
           <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-50} duration={1000} className="desktopMenuListItem">Contact</Link>
         </div>
 
-        <svg className='burgerMenu' onClick={()=>setShowMenu(!showMenu)} width="70px" height="70px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#FFFFFF" transform="matrix(-1, 0, 0, 1, 0, 0)">
+        <svg className='burgerMenu' onClick={()=>setShowMenu(!showMenu)} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#FFFFFF" transform="matrix(-1, 0, 0, 1, 0, 0)">
             <g id="SVGRepo_bgCarrier" strokeWidth="0"/>
             <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"/>
             <g id="SVGRepo_iconCarrier">
