fbf730df61a6f1e5dd005815a17f9f5b6d7f1c8b
teddyyyyx <benztagle123@Gmail.com>
2024-06-22 18:40:31 +0800

    Remove motion in p tag

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -25,16 +25,11 @@ export const About = () => {
         <hr className='about-hr'/>
 
         <div className='about-paragraph'>
-            <motion.p
-                initial={{ scale: 0.8, opacity: 0,}}
-                whileInView={{ scale: 1, opacity: 1,}}
-                transition={{ duration: .5}}
-                // viewport={{ once: true }}
-            >
+            <p>
               "Hey there! I'm Benz Samson Tagle, a passionate developer specializing in web development. 
               I hold a degree in Computer Science from Bicol University,
               where I cultivated my love for coding and technology. 
-            </motion.p>
+            </p>
   
             <p>
               My journey in web development has been driven by a desire to create impactful, 
