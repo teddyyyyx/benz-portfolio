51e76caaf55ee4451c000db6a0086f5d6ba8cd95
teddyyyyx <benztagle123@Gmail.com>
2024-06-29 14:52:48 +0800

    Added framer in about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,8 +9,12 @@ export const About = () => {
     <section id='about'>
             <h1 className='title'>About Me</h1>
 
-        <div className='about-container'>
-
+        <motion.div 
+          className='about-container'
+          initial={{ scale: 0.8, opacity: 0,}}
+          whileInView={{ scale: 1, opacity: 1,}}
+          transition={{ duration: 1}}
+        >
             <img src={devDudeImg} alt="" className='about-img'/>
 
             <div className='about-info'>
@@ -21,20 +25,28 @@ export const About = () => {
                 <p><span>Contact #:</span>  095  1621 9997</p>
                 <p><span>GitHub:</span>  github.com/teddyyyyx</p>
             </div>
-        </div>
+        </motion.div>
         <hr className='about-hr'/>
 
         <div className='about-paragraph'>
-            <p>
+            <motion.p
+             initial={{ scale: 0.8, opacity: 0,}}
+             whileInView={{ scale: 1, opacity: 1,}}
+             transition={{ duration: 1}}
+            >
               "Hey there! I'm Benz Samson Tagle, a passionate developer specializing in web development. 
               I hold a degree in Computer Science from Bicol University,
               where I cultivated my love for coding and technology. 
-            </p>
+            </motion.p>
   
-            <p>
+            <motion.p
+             initial={{ scale: 0.8, opacity: 0,}}
+             whileInView={{ scale: 1, opacity: 1,}}
+             transition={{ duration: 1.3}}
+            >
               My journey in web development has been driven by a desire to create impactful, 
               user-friendly web applications that solve real-world problems."
-            </p>
+            </motion.p>
             
            <br />
             x
