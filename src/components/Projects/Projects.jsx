752b4bc0b74b5767e3b0de792cb8dfd3bca33aec
teddyyyyx <benztagle123@Gmail.com>
2024-07-20 22:27:25 +0800

    Fixed Project component UI

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -39,17 +39,12 @@ export const Projects = () => {
       <hr className='projects-hr'/>
 
 {/* */}
-      <motion.div className='projects-container'  
+      <motion.div className='projects-container col-reverse'  
         initial={{ scale: 0.8, opacity: 0,}}
         whileInView={{ scale: 1, opacity: 1,}}
         transition={{ duration: 0}}
         viewport={{ once: true }}
       >
-          <a href="https://teddyyyyx.github.io/hanzy-stylez-react/" target='__blank'>
-            <img src={prj1} alt="" className='project-img'/>
-            <p className='visit-site-left'>Visit website</p>
-          </a>
-
             <div>
                 <a href="https://teddyyyyx.github.io/hanzy-stylez-react/" target='__blank'>
                   <h1>Hanzy Styles</h1>
@@ -58,44 +53,47 @@ export const Projects = () => {
                 crafting dynamic and responsive user interfaces for all modern devices. 
                 Applied my frontend expertise to ensure optimal performance and user experience.
             </div>
+
+            <a href="https://teddyyyyx.github.io/hanzy-stylez-react/" target='__blank'>
+            <img src={prj1} alt="" className='project-img'/>
+            <p className='visit-site-left'>Visit website</p>
+          </a>
       </motion.div>
       <hr className='projects-hr'/>
 
 {/* */}
 
-      <motion.div className='projects-container col-reverse'
+      <motion.div className='projects-container'
         initial={{ scale: 0.8, opacity: 0 }}
         whileInView={{ scale: 1, opacity: 1 }}
         transition={{ duration: 0 }}
         viewport={{ once: true }}
       >
-            <div>
-            <a href="https://teddyyyyx.github.io/movieflix-react/" target='__blank'> 
-              <h1>HoneyFlix</h1>
-            </a>
-              Crafted with love using ReactJS and fetched movie data from the Open Movie Database API. 
-              This website is designed for you to search your favorite movie and its information.
-              Ensuring compatibility and responsiveness that is truly
-              compatible for all modern viewports.          
-            </div>
-
             <a href="https://teddyyyyx.github.io/movieflix-react/" target='__blank'> 
               <img src={prj2} alt="" className='project-img1'/>
               <p className='visit-site-right'>Visit website</p>
             </a>
+
+            <div>
+              <a href="https://teddyyyyx.github.io/movieflix-react/" target='__blank'> 
+                <h1>HoneyFlix</h1>
+              </a>
+                Crafted with love using ReactJS and fetched movie data from the Open Movie Database API. 
+                This website is designed for you to search your favorite movie and its information.
+                Ensuring compatibility and responsiveness that is truly
+                compatible for all modern viewports.          
+            </div>
       </motion.div>
       <hr className='projects-hr'/>
 
 {/*  */}
 
-      <motion.div className='projects-container'
+      <motion.div className='projects-container col-reverse'
         initial={{ scale: 0.8, opacity: 0,}}
         whileInView={{ scale: 1, opacity: 1,}}
         transition={{ duration: 0}}
         viewport={{ once: true }}
       >
-        <img src={prj3} alt="" className='project-img'/>
-
           <div>
             <h1>Personal Website</h1>
             I built my personal portfolio website using ReactJS, showcasing 
@@ -104,6 +102,8 @@ export const Projects = () => {
             This approach allowed me to create a dynamic and interactive user experience,
             emphasizing modularity and code reusability.
           </div>
+
+          <img src={prj3} alt="" className='project-img'/>
       </motion.div>
     </section>
   )
